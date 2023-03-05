53e69fa3686e8384290af86d6bc3a35790262ac9
Nikita Semyachkin <93248541+guisecreator@users.noreply.github.com>
2023-03-05 22:24:37 +0500

    update models

--- main/models.py
+++ main/models.py
@@ -21,32 +21,73 @@ class Profiles(models.Model):
         ('ACC', 'Narration_Accepted'),
     )
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
-    narrate = models.TextField(max_length=9999, default='Enter Narration')
-    review_narration = models.CharField(max_length=50, choices=NARRATESTATUS, default='Reviewing Narration')
-    date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='', blank=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name='profile', 
+        )
+    
+    narrate = models.TextField(
+        max_length=9999, 
+        default='Enter Narration',
+        )
+    
+    review_narration = models.CharField(
+        max_length=50, 
+        choices=NARRATESTATUS, 
+        default='Reviewing Narration',
+        )
+    
+    date_of_birth = models.DateField(
+        blank=True, 
+        null=True,
+        )
+    
+    photo = models.ImageField(
+        upload_to='', 
+        blank=True,
+        )
 
     def __str__(self):
         return 'profile for user {}'.format(self.user.username)
     
-#class SimpleNote(models.Model):
+# class SimpleNote(models.Model):
 #    """Create new Simple note"""
-#    name = models.CharField(max_length=50, verbose_name='Name note')
-#    text = models.CharField(max_length=10000000, verbose_name='Note')
-#    images = models.FileField(verbose_name='Upload file')
-#    time_create = models.DateTimeField(auto_now_add=True, 
-#                                       verbose_name='Time add in time_create')
-#    time_update = models.DateTimeField(auto_now=True, 
-#                                       verbose_name='Time update in time_create')
+
+#    name = models.CharField(
+#        max_length=50, 
+#        verbose_name='Name note',
+#        )
+   
+#    text = models.CharField(
+#        max_length=10000000, 
+#        verbose_name='Note',
+#        )
+   
+#    images = models.FileField(
+#        verbose_name='Upload file'
+#        )
+
+#    time_create = models.DateTimeField(
+#        auto_now_add=True, 
+#        verbose_name='Time add in time_create',
+#        )
+   
+#    time_update = models.DateTimeField(
+#        auto_now=True, 
+#        verbose_name='Time update in time_create',
+#        )
+   
 #    is_printing = models.BooleanField(default=True)
-#
+
 #    def if_save_note(self, *args, **kwargs):
 #        ...
-#
+
 #    def __str__(self):
 #        return self.name
-#    
+   
 #    class Meta:
 #        verbose_name = 'Simple Note'
 #        ordering = ['time_create', 'name']
+
+
