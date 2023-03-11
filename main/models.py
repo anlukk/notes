7f541034befe74e851939fdc98f67b90b56dc7bf
Nikita Semyachkin <93248541+guisecreator@users.noreply.github.com>
2023-03-11 18:01:20 +0500

    before migrations and + icons

--- main/models.py
+++ main/models.py
@@ -99,3 +99,29 @@ class SimpleNote(models.Model):
    class Meta:
        verbose_name = 'Simple Note'
        ordering = ['time_create', 'name']
+
+class Category(models.Model):   #Change this
+
+    name = models.CharField(
+        max_length=100, 
+        db_index=True, 
+        verbose_name="Category"
+        )
+    
+    slug = models.SlugField(
+        max_length=255, 
+        unique=True, 
+        db_index=True, 
+        verbose_name="URL"
+        )
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categorys'
+        ordering = ['id']
