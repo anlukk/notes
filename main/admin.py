158e93526bfc1f296fc53c966d29d1994eadfcd1
Nikita Semyachkin <93248541+guisecreator@users.noreply.github.com>
2023-03-12 21:53:35 +0500

    Simple Note in admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Task, Profiles
+from .models import *
 
 admin.site.register(Task)
 
@@ -7,3 +7,20 @@ admin.site.register(Task)
 @admin.register(Profiles)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'date_of_birth']
+
+
+class SimpleNoteAdmin(admin.ModelAdmin):
+    list_display = (
+        'file_path',
+        'file_note',
+        'time_create',
+        'time_update',
+        'is_printing',
+        'name', 
+        'text', 
+        'slug',
+        )
+
+admin.site.register(SimpleNote, SimpleNoteAdmin)
+
+
